8cc38e4efdb2a153469a5f278cec50f6e9be6b4d
terru3 <terru3@hotmail.com>
2025-06-28 23:33:55 -0700

    change modes to dev and use in py

--- use_submodule.py
+++ use_submodule.py
@@ -1,3 +1,6 @@
-from submodule.models import Repository
+from submodule.models import Repository, Env
 
 bruh = Repository(repo_name="yuh", repo_function="type sh")
+
+env = Env.DEV
+print(f"Env: {env}")
